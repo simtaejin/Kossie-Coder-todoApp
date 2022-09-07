a7f58d1b047dc749cc853288b0c2a96d73ebe7c3
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-09-07 22:20:46 +0900

    01-07 : To-Do 추가 폼 만들기

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="name">
+    {{ name }}
+  </div>
+  <button
+      class="btn btn-primary"
+      v-on:click="updateName"
+  >Click</button>
+</template>
+
+<script>
+  import { ref} from "vue";
+
+  export default {
+    setup() {
+       let name = ref({
+         id: 1
+       });
+
+       // const greeting = (name) => {
+       //   return 'Hello, ' + name;
+       // };
+       //
+       // const greet = greeting(name);
+
+       const updateName = () => {
+         name.value.id = 2;
+         console.log(name);
+       }
+
+       return {
+         name,
+         updateName
+       };
+    }
+  }
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>
